bbaf774c9d4e99dc9866292b1b72d5c2e007e201
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2023-10-22 10:50:43 +0200

    stop showing status bar in nvim-tree

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -7,8 +7,8 @@ local config = {
     component_separators = { left = '', right = ''},
     section_separators = { left = '', right = ''},
     disabled_filetypes = {
-      statusline = {},
-      winbar = {},
+      statusline = {'NvimTree'},
+      winbar = {'NvimTree'},
     },
     ignore_focus = {},
     always_divide_middle = true,
