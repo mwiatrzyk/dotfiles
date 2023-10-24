e1fa6f46279cad84bdabf4c26932cae9f3611961
Maciej Wiatrzyk <maciej.wiatrzyk@nokia.com>
2023-10-24 08:53:31 +0200

    conf(nvim): disable ignorecase and enable hlsearch

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -1,25 +1,27 @@
+local opt = vim.opt
+
 -- Hint: use `:h <option>` to figure out the meaning if needed
-vim.opt.clipboard = 'unnamedplus'   -- use system clipboard
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
-vim.opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
+opt.clipboard = 'unnamedplus'   -- use system clipboard
+opt.completeopt = {'menu', 'menuone', 'noselect'}
+opt.mouse = 'a'                 -- allow the mouse to be used in Nvim
 
 -- Tab
-vim.opt.tabstop = 4                 -- number of visual spaces per TAB
-vim.opt.softtabstop = 4             -- number of spacesin tab when editing
-vim.opt.shiftwidth = 4              -- insert 4 spaces on a tab
-vim.opt.expandtab = true            -- use spaces for tabs
+opt.tabstop = 4                 -- number of visual spaces per TAB
+opt.softtabstop = 4             -- number of spacesin tab when editing
+opt.shiftwidth = 4              -- insert 4 spaces on a tab
+opt.expandtab = true            -- use spaces for tabs
 
 -- UI config
-vim.opt.number = true               -- show absolute number
-vim.opt.relativenumber = true       -- add numbers to each line on the left side
-vim.opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
-vim.opt.splitbelow = true           -- open new vertical split bottom
-vim.opt.splitright = true           -- open new horizontal splits right
+opt.number = true               -- show absolute number
+opt.relativenumber = true       -- add numbers to each line on the left side
+opt.cursorline = true           -- highlight cursor line underneath the cursor horizontally
+opt.splitbelow = true           -- open new vertical split bottom
+opt.splitright = true           -- open new horizontal splits right
 -- vim.opt.termguicolors = true        -- enabl 24-bit RGB color in the TUI
-vim.opt.showmode = false            -- we are experienced, wo don't need the "-- INSERT --" mode hint
+opt.showmode = false            -- we are experienced, wo don't need the "-- INSERT --" mode hint
 
 -- Searching
-vim.opt.incsearch = true            -- search as characters are entered
-vim.opt.hlsearch = false            -- do not highlight matches
-vim.opt.ignorecase = true           -- ignore case in searches by default
-vim.opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
+opt.incsearch = true            -- search as characters are entered
+opt.hlsearch = true             -- highlight matches
+--opt.ignorecase = true           -- ignore case in searches by default
+--opt.smartcase = true            -- but make it case sensitive if an uppercase is entered
