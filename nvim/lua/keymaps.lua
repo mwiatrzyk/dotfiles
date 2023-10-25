ec629e04a613f82c42712c4acb3460c570ced98b
Maciej Wiatrzyk <maciej.wiatrzyk@nokia.com>
2023-10-25 09:45:53 +0200

    conf(nvim): exchange the meaning of Ctrl+hjkl and Ctrl+arrow
    I find more often to move between windows instead of resizing them, so
    f.e. having Ctrl+Left to move to the left window is more handy for me
    than to use Ctrl+h to do the same. Since I can resize windows via mouse,
    these bindings can be exchanged

--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -12,17 +12,17 @@ local opts = {
 
 -- Hint: see `:h vim.map.set()`
 -- Better window navigation
-vim.keymap.set('n', '<C-h>', '<C-w>h', opts)
-vim.keymap.set('n', '<C-j>', '<C-w>j', opts)
-vim.keymap.set('n', '<C-k>', '<C-w>k', opts)
-vim.keymap.set('n', '<C-l>', '<C-w>l', opts)
+vim.keymap.set('n', '<C-Left>', '<C-w>h', opts)
+vim.keymap.set('n', '<C-Down>', '<C-w>j', opts)
+vim.keymap.set('n', '<C-Up>', '<C-w>k', opts)
+vim.keymap.set('n', '<C-Right>', '<C-w>l', opts)
 
 -- Resize with arrows
 -- delta: 2 lines
-vim.keymap.set('n', '<C-Up>', ':resize -2<CR>', opts)
-vim.keymap.set('n', '<C-Down>', ':resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Left>', ':vertical resize +2<CR>', opts)
-vim.keymap.set('n', '<C-Right>', ':vertical resize -2<CR>', opts)
+vim.keymap.set('n', '<C-k>', ':resize -2<CR>', opts)
+vim.keymap.set('n', '<C-j>', ':resize +2<CR>', opts)
+vim.keymap.set('n', '<C-h>', ':vertical resize +2<CR>', opts)
+vim.keymap.set('n', '<C-l>', ':vertical resize -2<CR>', opts)
 
 -- Toggle nvim.tree
 vim.keymap.set('n', '<F3>', ':NvimTreeToggle<CR>', opts)
