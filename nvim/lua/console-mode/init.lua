e5a0492a0deda854fe9ba69abf47742ef8c7ff95
Maciej Wiatrzyk <maciej.wiatrzyk@gmail.com>
2025-11-08 08:10:21 +0100

    feat(nvim): upgrade deps and add `venv-selector` plugin

--- nvim/lua/console-mode/init.lua
+++ nvim/lua/console-mode/init.lua
@@ -136,6 +136,23 @@ require("lazy").setup({
     "danymat/neogen",
     config = true,
     version = "*"
+  },
+  -- Plugin for Python venv selection
+  {
+    "linux-cultist/venv-selector.nvim",
+    dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
+    opts = {
+      -- Your options go here
+      -- name = "venv",
+      -- auto_refresh = false
+    },
+    event = "VeryLazy", -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+    keys = {{
+      -- Keymap to open VenvSelector to pick a venv.
+      "<leader>vs", "<cmd>:VenvSelect<cr>",
+      -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+      "<leader>vc", "<cmd>:VenvSelectCached<cr>"
+    }}
   }
 })
 
